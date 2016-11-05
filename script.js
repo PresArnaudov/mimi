9ed28fae7d91dae6577f42c63b9c65b4fb9cc1f3
PetarAD <petar.ad@mail.bg>
2016-11-05 23:56:50 +0200

    change

--- script.js
+++ script.js
@@ -1,59 +1,75 @@
 var n;
-var up=0;
+var up=1;
+var i;
+
+//take n
+
 function getThis(event) {
   event.preventDefault()
-   n =document.getElementById('n').value;
-  console.log(n);
+  n =document.getElementById('n').value;
+  //console.log(n);
   makeForm(n);
 }
 
+//create form
+
 function makeForm(n){
-var form = document.createElement('form');
-form.setAttribute('method',"post");
-form.setAttribute('action',"index.html");
+  var form = document.createElement('form');
+  form.setAttribute('method',"post");
+  form.setAttribute('action',"index.html");
+  while (up<=n) {
+    i = document.createElement("input"); //input element, text
+    i.setAttribute('type',"text");
+    i.setAttribute('id',"comand"+up);
+    form.appendChild(i);
+    up++;
+  }
+  var s = document.createElement("input"); //input element, Submit button
+  s.setAttribute('type',"submit");
+  s.setAttribute('value',"Submit");
+  s.setAttribute('onclick',"getComand(event)");
+  form.appendChild(s);
+  document.getElementsByTagName('body')[0].appendChild(form);
+}
+
+// take comands
+
+function getComand(event){
+  event.preventDefault();
+  var up=1
 while (up<=n) {
-  var i = document.createElement("input"); //input element, text
-  i.setAttribute('type',"text");
-  i.setAttribute('id',"comand".n);
-  form.appendChild(i);
+ console.log(document.getElementById('comand'+up).value);
+  up++;
 }
-var s = document.createElement("input"); //input element, Submit button
-s.setAttribute('type',"submit");
-s.setAttribute('value',"Submit");
-form.appendChild(s);
 }
 
+//make second form for values of arry
 
+function makeFormSecond(){
 
+}
 
-// var par;
-// par=document.getElementById('arr').value;
-  //1<=n<=30000
-  //x =1/2
-  //1= put in
-  //2 = take
+//create function
+
 function safe(param) {
   var  helper=[];
   var n=param[0];
   if (n<1 || n>30000) {
     console.log("off parameters");
   }
-for (var i = 1; i < param.length; i++) {
-// console.log(param[i]);
-if (param[i]==2) {
-  console.log(Math.max(...helper));
-}
-for (var j = 0; j < param.length; j++) {
-  // console.log(param[i][j]);
-     if (param[i][j-1]==1) {
-       helper.push(param[i][j])
-     }
-}
-}
+  for (var i = 1; i < param.length; i++) {
+    if (param[i]==2) {
+      console.log(Math.max(...helper));
+    }
+  }
+  for (var j = 0; j < param.length; j++) {
+    if (param[i][j-1]==1) {
+     helper.push(param[i][j])
+    }
+  }
 console.log(helper);
 }
-safe([6,[1,2],[1,4],2,[1,8],[1,7],2]);
-console.log('next');
-safe([6,[1,40],[1,8],2,[1,30],[1,35],2]);
-makeForm(2);
+// safe([6,[1,2],[1,4],2,[1,8],[1,7],2]);
+// console.log('next');
+// safe([6,[1,40],[1,8],2,[1,30],[1,35],2]);
 // safe(par);
